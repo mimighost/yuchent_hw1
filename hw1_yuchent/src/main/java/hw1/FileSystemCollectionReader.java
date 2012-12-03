34e8d4ceefd11995eacde6819d37824f5c3b2f42
mimighost <taylortianyuchen@gmail.com>
2012-12-03 17:58:35 -0500

    collection

--- hw1_yuchent/src/main/java/hw1/FileSystemCollectionReader.java
+++ hw1_yuchent/src/main/java/hw1/FileSystemCollectionReader.java
@@ -1,22 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package hw1;
 
 import java.io.File;
@@ -27,7 +8,6 @@ import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader_ImplBase;
-import hw1.SourceDocumentInformation;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.DocumentAnnotation;
 import org.apache.uima.resource.ResourceConfigurationException;
@@ -98,10 +78,11 @@ public class FileSystemCollectionReader extends CollectionReader_ImplBase {
 
     // if input directory does not exist or is not a directory, throw exception
     
+    /*
     if (!directory.exists()) {
       throw new ResourceInitializationException(ResourceConfigurationException.DIRECTORY_NOT_FOUND,
               new Object[] { PARAM_INPUTDIR, this.getMetaData().getName(), directory.getPath() });
-    }
+    }*/
 
     // get list of files in the specified directory, and subdirectories if the
     // parameter PARAM_SUBDIR is set to True
